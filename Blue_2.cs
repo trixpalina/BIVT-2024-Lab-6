2a52bcb53d60c20a2be0a3b52b7ee53392cdf544
trixpalina <124019361+trixpalina@users.noreply.github.com>
2025-03-04 13:00:45 +0300

    Update Blue_2.cs

--- Blue_2.cs
+++ Blue_2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,54 +16,58 @@ namespace Lab_6
             private string _name;
             private string _surname;
             private int[,] _marks;
-
+            private int _count;
             //публичные свойства
-            private string Name => _name;
-            private string Surname => _surname;
-            private int[,] Marks => _marks;
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[,] Marks
+            {
+                get
+                {
+                    if (_marks == null) return null;
+                    int[,] r = new int[_marks.GetLength(0), _marks.GetLength(1)];
+                    for (int i = 0; i < _marks.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < _marks.GetLength(1); j++)
+                        {
+                            r[i, j] = _marks[i, j];
+                        }
+                    }
+                    return r;
 
+                }
+            }
             //публичное поле
             public int TotalScore
             {
                 get
-                {
-                    int sum = 0; 
-                    foreach (var mark in _marks)   
-                        sum += mark;               
-                    return sum;                    
+                { if (_marks == null) return 0;
+
+                    int sum = 0;
+                    foreach (var mark in _marks)
+                        sum += mark;
+                    return sum;
                 }
             }
-
             //конструктор
-            public Participant (string name, string surname)
+            public Participant(string name, string surname)
             {
                 _name = name;
                 _surname = surname;
                 _marks = new int[2, 5];
-
+                _count = 0;
             }
-
             public void Jump(int[] result)
             {
+                if (result == null ) return;
                 if (_marks == null) return;
-
-                for (int i = 0; i < _marks.GetLength(0); i++)
+                for (int i = 0; i < 5; i++)
                 {
-                    if (_marks[i, 0] != 0)
-                    {
-                        continue;
-                    }
-
-
-                    for (int j = 0; j < _marks.GetLength(1); j++)
-                    {
-                        _marks[i, j] = result[j];
-                    }
-                    break;
+                    _marks[_count, i] = result[i];
                 }
+                _count++;
             }
-
-
+            
             public static void Sort(Participant[] array)
             {
                 if (array == null || array.Length == 0) return;
@@ -75,34 +79,31 @@ namespace Lab_6
                         {
                             (array[j], array[j + 1]) = (array[j + 1], array[j]);
                         }
-
                     }
                 }
-
             }
-
-
+           
             public void Print()
+        {
+            Console.Write("Name: ");
+            Console.WriteLine(_name);
+
+            Console.Write("Surname: ");
+            Console.WriteLine(_surname);
+
+            Console.Write("Marks: ");
+            for (int i = 0; i < _marks.GetLength(0); i++)
             {
-                Console.Write("Name: ");
-                Console.WriteLine(_name);
-
-                Console.Write("Surname: ");
-                Console.WriteLine(_surname);
-
-                Console.Write("Marks: ");
-                for (int i = 0; i < _marks.GetLength(0); i++)
+                for (int j = 0; j < _marks.GetLength(1); j++)
                 {
-                    for (int j = 0; j < _marks.GetLength(1); j++)
-                    {
-                        Console.Write(_marks[i, j]);
-                        Console.Write(" ");
-                    }
-                    Console.WriteLine();
+                    Console.Write(_marks[i, j]);
+                    Console.Write(" ");
                 }
+                Console.WriteLine();
             }
-
-    
         }
+
+
+    }
     }
 }
